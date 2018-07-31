7c0f5ab3ee75c76d3ebd3a927ceb83a637436840
Mara_Work <mara_work@mga.fritz.box>
2018-07-31 17:00:44 +0200

    added waypoints

--- Example/index.js
+++ Example/index.js
@@ -1,17 +1,21 @@
+let directionsDisplay
+let directionsService
+let map
+
 function initMap() {
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     center: { lat: 41.85, lng: -87.65 }
   });
   directionsDisplay.setMap(map);
-  calculateAndDisplayRoute()
-
+}
 
 
   function calculateAndDisplayRoute() {
     navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('got position')
      var origin = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
     directionsService.route({
       origin,
@@ -26,4 +30,29 @@ function initMap() {
       });
     });
   }
-}
+
+document.getElementById('submit').addEventListener('click', event=>{
+  event.preventDefault()
+  console.log('click')
+  calculateAndDisplayRoute()
+})
+
+
+
+  function calculateAndDisplayRoute() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('got position')
+     var origin = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    directionsService.route({
+      origin,
+      destination: 'Rom',
+      travelMode: 'DRIVING'
+    }, function (response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    });
+  }
